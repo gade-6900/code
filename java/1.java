ca7372b4390921834dff687bdbd6bedcf3188a6f
gade-6900 <gadepatil438@gmail.com>
2024-08-24 09:47:04 +0530

    add

--- java/1.java
+++
@@ -1,6 +0,0 @@
-class FirstClass {
-    public static void main(String args[]){
-       System.out.println( "hello world");  
-
-    }
-}
--- java/hello.java
+++
@@ -1,8 +0,0 @@
-class Hello
-{
-    public static void main(String a[])
-    {
-        System.out.print("hello world");
-
-    }
-}
