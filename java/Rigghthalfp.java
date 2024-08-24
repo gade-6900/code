1f0e8da6d18c880df2aa7a8c13bf2f92d82cab5b
gade-6900 <gadepatil438@gmail.com>
2024-08-24 20:04:32 +0530

    add

---
+++ java/Rigghthalfp.java
@@ -0,0 +1,9 @@
+public class Rigghthalfp {
+    public static void main(String[] args) {
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
---
+++ java/patterns.java
@@ -0,0 +1,10 @@
+public class patterns {
+    public static void main (String[]args){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+   
+}
+}
