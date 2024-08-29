42c1b46834177ed3357f7864e8023bdb0174468a
gade-6900 <gadepatil438@gmail.com>
2024-08-29 22:05:19 +0530

    new

---
+++ java/Swap.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcone to the Swapping station\n\n");
+        System.out.print("Enter value of A: ");
+        int a = input.nextInt();
+        System.out.print("Enter value of B: ");
+        int b = input.nextInt();
+
+        int c = a;
+        a = b;
+        b = c;
+
+        System.out.println("value of A is:" + a);
+        System.out.println("value of B is:" + b);
+
+
+
+
+
+
+    }
+    
+}
---
+++ java/asssingment.java
@@ -0,0 +1,10 @@
+public class asssingment {
+    public static void main(String[] args) {
+        int myInt = 19;
+        System.out.println(myInt);
+
+        int newInt = myInt;
+        System.out.println(newInt);
+    }
+    
+}
