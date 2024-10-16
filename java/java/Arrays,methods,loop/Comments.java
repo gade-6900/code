8cc517d1f6bad9d077fa338f8492a026b672f0e5
gade-6900 <gadepatil438@gmail.com>
2024-10-16 13:35:20 +0530

    new

--- java/java/Arrays,methods,loop/Comments.java
+++ java/java/Arrays,methods,loop/Comments.java
@@ -1,4 +1,7 @@
 public class Comments{
+    /**
+     * @gade-6900 args this is the java doc  comments  
+     */
     public static void main(String[] args) {
         // i have to say something to you 
         System.out.println("hello guys how are you\n");
@@ -7,12 +10,7 @@ public class Comments{
 
         System.out.println("In Java,are the main keywords\n in Java, categorized by their use:");
         
-       /** int a = 10;
-        int b = 20;
-        int c ; 
-        c = a + b;
-        */
-        /** java doc coments this is shown in the above code  */
-
+      
+        
     }
 }
